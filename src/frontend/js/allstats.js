0e149f97bbcb392f2106c93f75439c419808577f
MaximGaskov <maskimgaskov@mail.ru>
2018-12-14 23:17:24 +0300

    add avg difficulty graphic

--- src/frontend/js/allstats.js
+++ src/frontend/js/allstats.js
@@ -51,6 +51,25 @@ function requestTopAuth() {
     }
 }
 
+function requestAvgDifficulty() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/AvgDifficultyServlet", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+    
+        if (this.readyState == 4 && this.status == 200) {
+            var jsonData = JSON.parse(this.responseText);
+            console.log("succsess");
+            
+            drawGraphic(jsonData);
+    
+        } else {
+            console.log(this.status)
+        }   
+    }
+}
 
 function insertTableRow(title, author, difficulty, id, table) {
     var newRow;
@@ -85,6 +104,59 @@ function insertTableRow(title, author, difficulty, id, table) {
     }
 
     containerBlock.appendChild( newRow );
+}
+
+function drawGraphic(dataArray){
+    var xArr = new Array();
+    var yArr = new Array();
+
+    var arrayLength;
+
+    if (dataArray.length > 52){
+        arrayLength = 52;
+    } else {
+        arrayLength = dataArray.length;
+    }
+
+    for (var i = 0; i < arrayLength; i++) {
+        yArr[i] = dataArray[i];
+        xArr[i] = 1500 + i * 10;
+    }
+
+    var trace1 = {
+
+        y: yArr,
+        x: xArr,
+        type: 'scatter'
+    };
+
+
+    var data = [trace1];
+
+    var layout = {
+        title: "<b>Average book's difficulty</b>",
+        titlefont: {
+            size: 20
+        },
+        xaxis: {
+        title: 'year',
+        titlefont: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'
+        }
+        },
+        yaxis: {
+        title: 'difficulty',
+        titlefont: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'
+        }
+        }
+    };
+
+    Plotly.newPlot('graphId', data, layout);
 }
 
 document.getElementById("bookBtn").onclick = function() {
